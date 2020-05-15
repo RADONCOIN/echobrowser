c0f19caa7e80b31e2dd382cfb76c36a45f17cd93
Jelgerspar <65359968+Jelgerspar@users.noreply.github.com>
2020-05-15 17:59:05 +0200

    Fixed some bugs for the empty string error
    empty search strings had good SEO

--- echobrowser-master/crawledpythontojson/crawledpythonnaarjson.js
+++ echobrowser-master/crawledpythontojson/crawledpythonnaarjson.js
@@ -27,15 +27,28 @@ for (i = 2; i < res.length; i++) {
  	e=e.split('/');
 
 	console.log(e)
-	if(e[0]===undefined){
-		e.slice([0[0]])
+	if(e[0]===null || e[0]==="" || e[0]===undefined || e[0]===" " || e[0]===false||e[0]==="false"){
+		e[0]="*^"
 		console.log('sliced!')
+	}
+		if(e[1]===null || e[1]==="" || e[1]===undefined || e[1]===" " || e[1]===false||e[1]==="false"){
+		e[1]="*^"
+		console.log('sliced!')
+	}
+	if(e[2]===null || e[2]==="" || e[2]===undefined || e[2]===" " || e[2]===false||e[2]==="false"){
+		e[2]="*^"
+		console.log('sliced')
 	}
 
 	
 	resultaat = resultaat +  "{'vertaling':"+"'https://"+res[i]+"','stam':'"+e[0]+"','stam2':'"+e[1]+"','stam3':'"+e[2]+"','tekstlink':'"+res[i]+"'},";
  	// https://pt-br.facebook.com/pages/category/dvd-video-store/
 	resultaat = resultaat.replace(/\s/g, '');
+
+	resultaat = resultaat.replace(/,'stam3':''/g, "");
+	resultaat = resultaat.replace(/,'stam2':''/g, "");
+	resultaat = resultaat.replace(/,'stam1':''/g, "");
+
 	$( "#tekst" ).text(resultaat);
 }
 }
