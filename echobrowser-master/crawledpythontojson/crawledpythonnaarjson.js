aeddb22d9f3a4f5cd1808cbd4d317e68005a0549
Jelgerspar <65359968+Jelgerspar@users.noreply.github.com>
2020-05-15 20:47:50 +0200

    removed domain extentions and added http support

--- echobrowser-master/crawledpythontojson/crawledpythonnaarjson.js
+++ echobrowser-master/crawledpythontojson/crawledpythonnaarjson.js
@@ -6,7 +6,10 @@ var resultaat="";
 var result2;
 	var str = prompt("input graag")
 
-	var res = str.split('https://');
+
+
+	var res = str.split('\n');
+		
 
 	console.log(res)
 	var i;
@@ -16,14 +19,31 @@ for (i = 2; i < res.length; i++) {
 
 
  	var e=res[i].split('.')
-
+var extentionsdelete = [",nl", ",com", ",us",",be", ",ch", ",edu",",net", ",org", ",co",",info", ",uk", ",cn",",shop", ",store", ",gay",",game", ",ru", ",eu",",cx", ",to", ",ly",",gl", ",link", ",me", ",ffm",","];
  	e=e.toString();
- 	e=e.replace("www", "");
- 	e=e.replace(',',"")
- 	e=e.replace(',',"")
- 		e=e.replace(',',"")
- 			e=e.replace(',',"")
- 	e=e.replace('com',"")
+
+
+ 	e=e.replace("www,", "");
+ 	e=e.replace("https://", "");
+ 		e=e.replace("http://", "");
+
+
+ 		// e=e.replace(',com',"")
+ 	 // 	e=e.replace(',ch',"")
+ 	 // 	e=e.replace(',org',"")
+ 	 // 	e=e.replace(',net',"")
+ 	 // 	e=e.replace(',co',"")
+ 	 // 	e=e.replace(',de',"")
+ 	 // 	e=e.replace(',nl',"")
+ 	 // 	e=e.replace(',us',"")
+ 	 // 	e=e.replace(',be',"")
+ 	 // 	e=e.replace(',edu',"")
+
+ 	var k;
+		for ( k = 0; k < extentionsdelete.length; k++) {
+		 	e=e.replace(extentionsdelete[k],"")
+		    console.log('hoi')
+		}
  	e=e.split('/');
 
 	console.log(e)
@@ -41,13 +61,13 @@ for (i = 2; i < res.length; i++) {
 	}
 
 	
-	resultaat = resultaat +  "{'vertaling':"+"'https://"+res[i]+"','stam':'"+e[0]+"','stam2':'"+e[1]+"','stam3':'"+e[2]+"','tekstlink':'"+res[i]+"'},";
+	resultaat = resultaat +  "{'vertaling':"+"'"+res[i]+"','stam':'"+e[0]+"','stam2':'"+e[1]+"','stam3':'"+e[2]+"','tekstlink':'"+res[i]+"'},";
  	// https://pt-br.facebook.com/pages/category/dvd-video-store/
 	resultaat = resultaat.replace(/\s/g, '');
 
 	resultaat = resultaat.replace(/,'stam3':''/g, "");
 	resultaat = resultaat.replace(/,'stam2':''/g, "");
-	resultaat = resultaat.replace(/,'stam1':''/g, "");
+	resultaat = resultaat.replace(/,'stam':''/g, "");
 
 	$( "#tekst" ).text(resultaat);
 }
